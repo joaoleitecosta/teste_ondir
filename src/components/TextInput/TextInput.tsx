9dcb343a22da41ae5bdab7c6901ec663bedffd3e
Joao Leite <joao_leite@hotmail.com.br>
2024-11-29 17:19:19 -0300

    feat: add component TextInput

---
+++ src/components/TextInput/TextInput.tsx
@@ -0,0 +1,88 @@
+import type React from 'react';
+import {useRef} from 'react';
+import {
+  Pressable,
+  TextInput as RNTextInput,
+  type TextInputProps as RNTextInputProps,
+  type TextStyle,
+} from 'react-native';
+
+import {useAppTheme} from '@hooks';
+
+import {Box, type BoxProps} from '../Box/Box';
+import {$fontFamily, $fontSizes, Text} from '../Text/Text';
+
+export interface TextInputProps extends RNTextInputProps {
+  label?: string;
+  errorMessage?: string;
+  RightComponent?: React.ReactElement;
+  LeftComponent?: React.ReactElement;
+  boxProps?: BoxProps;
+}
+export function TextInput({
+  label,
+  errorMessage,
+  RightComponent,
+  LeftComponent,
+  boxProps,
+  ...rnTextInputProps
+}: TextInputProps) {
+  const {colors} = useAppTheme();
+  const inputRef = useRef<RNTextInput>(null);
+
+  const $textInputContainer: BoxProps = {
+    flexDirection: 'row',
+    borderWidth: errorMessage ? 2 : 1,
+    borderColor: errorMessage ? 'error' : 'gray4',
+    padding: 's16',
+    borderRadius: 's12',
+  };
+
+  function focusInput() {
+    inputRef.current?.focus();
+  }
+  return (
+    <Box flexGrow={1} flexShrink={1} {...boxProps}>
+      <Pressable onPress={focusInput}>
+        {label && (
+          <Text preset="paragraphMedium" marginBottom="s4">
+            {label}
+          </Text>
+        )}
+        <Box {...$textInputContainer}>
+          {LeftComponent && (
+            <Box justifyContent="center" mr="s16">
+              {LeftComponent}
+            </Box>
+          )}
+          <RNTextInput
+            autoCapitalize="none"
+            ref={inputRef}
+            placeholderTextColor={colors.gray2}
+            style={$textInputStyle}
+            {...rnTextInputProps}
+          />
+          {RightComponent && (
+            <Box justifyContent="center" ml="s16">
+              {RightComponent}
+            </Box>
+          )}
+        </Box>
+        {errorMessage && (
+          <Text color="error" preset="paragraphSmall" fontFamily="bold">
+            {errorMessage}
+          </Text>
+        )}
+      </Pressable>
+    </Box>
+  );
+}
+
+export const $textInputStyle: TextStyle = {
+  padding: 0,
+  flexGrow: 1,
+  flexShrink: 1,
+
+  ...$fontFamily.regular,
+  ...$fontSizes.paragraphMedium,
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export * from './Box/Box';
 export * from './Icon/Icon';
 export * from './Text/Text';
+export * from './TextInput/TextInput';
 export * from './ActivityIndicator/ActivityIndicator';
 
