3b1225c8820a967269456828440a23d986c434a4
Joao Leite <joao_leite@hotmail.com.br>
2024-11-29 14:48:28 -0300

    feat: add component ActivityIndicator

---
+++ src/components/ActivityIndicator/ActivityIndicator.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+	ActivityIndicator as RNActivityIndicator,
+	type ActivityIndicatorProps as RNActivityIndicatorProps,
+} from "react-native";
+
+import { useTheme } from "@shopify/restyle";
+
+import type { Theme, ThemeColors } from "@theme";
+
+interface ActivityIndicatorProps
+	extends Omit<RNActivityIndicatorProps, "color"> {
+	readonly color?: ThemeColors;
+}
+
+export function ActivityIndicator({
+	color = "primary",
+}: ActivityIndicatorProps) {
+	const { colors } = useTheme<Theme>();
+	return <RNActivityIndicator color={colors[color]} />;
+}
