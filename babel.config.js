499dfa9c830c73ef1b24178a6130864559df5ada
Joao Leite <joao_leite@hotmail.com.br>
2024-11-29 16:40:48 -0300

    chore: add relative path in babel

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: '.',
+        alias: {
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@assets': './src/assets',
+          '@hooks': './src/hooks',
+          '@theme': './src/theme',
+          '@domain': './src/domain',
+          '@utils': './src/utils',
+          '@services': './src/services',
+        },
+      },
+    ],
+  ],
 };
