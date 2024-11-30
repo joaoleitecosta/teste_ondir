802bf7d2a11fbc2ef37d8e5937b1ecb0d51c80df
Joao Leite <joao_leite@hotmail.com.br>
2024-11-30 13:51:22 -0300

    perf: update retuns

--- src/domain/User/userService.ts
+++ src/domain/User/userService.ts
@@ -2,22 +2,20 @@ import { storage } from '@services';
 import { User } from './userTypes';
 import { SignUpData } from '../Auth';
 
-async function getAllUser(): Promise<User[]> {
+async function getAllUser(): Promise<SignUpData[]> {
   const users: string | null = await storage.getItem('users');
 
-  const usersParse = users ? JSON.parse(users) : [];
+  return  users ? JSON.parse(users) : [];
 
-  return usersParse.map((user: SignUpData) => {
-    return {
-      email: user.email,
-      fullName: user.fullName,
-    };
-  });
+
 }
 
 async function getUserByEmail(email: string): Promise<User | undefined> {
   const users = await getAllUser();
-  const user = users.find((item: User) => item.email === email);
+  const user = users.find((item: User) => {
+    if(item.email === email) {
+      return {email: item.email, fullName: item.fullName};
+    }});
   return user;
 }
 
