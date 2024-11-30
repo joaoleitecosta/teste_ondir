e971cee0d86c57e609eeb1f85162b9af859ff818
Joao Leite <joao_leite@hotmail.com.br>
2024-11-30 13:52:01 -0300

    feat: add new querykeys

--- src/infra/infraTypes.ts
+++ src/infra/infraTypes.ts
@@ -2,4 +2,6 @@ export enum QueryKeys {
 	AuthSignUp = 'AuthSignUp',
 	AuthIsEmailAvailable = 'AuthIsEmailAvailable',
 	UserGetAll = 'UserGetAll',
+	AuthSignIn = 'AuthSignIn',
+	AuthSignOut = 'AuthSignOut',
 }
