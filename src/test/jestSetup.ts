a9b5c9b989fea0bb505066309c530cce86a1c006
Joao Leite <joao_leite@hotmail.com.br>
2024-11-30 21:26:05 -0300

    feat: create setup testes

---
+++ src/test/jestSetup.ts
@@ -0,0 +1,16 @@
+import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock';
+jest.mock('react-native-safe-area-context', () => ({
+  ...mockSafeAreaContext,
+  useSafeAreaInsets: jest.fn(mockSafeAreaContext.useSafeAreaInsets),
+}));
+
+jest.mock('@react-navigation/native', () => {
+  const originalModule = jest.requireActual('@react-navigation/native');
+  return {
+    ...originalModule,
+    useNavigation: () => ({
+      navigate: jest.fn(),
+      push: jest.fn(),
+    }),
+  };
+});
